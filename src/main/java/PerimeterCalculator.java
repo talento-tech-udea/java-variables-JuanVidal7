9b4a8dea0b2be67b9dbf934893ad535a00eccf7a
Juan Vidal <juancarlosvidalp@hotmail.com>
2024-09-11 17:49:15 -0700

    First Program PerimeterCalculator JV

--- src/main/java/PerimeterCalculator.java
+++ src/main/java/PerimeterCalculator.java
@@ -21,7 +21,7 @@ public class PerimeterCalculator {
         Point prevPoint = points.getLast();
 
         for (Point point : points) {
-            // Obtener la distancia de los lados esto es una prueba
+            // Obtener la distancia de los lados
             // Ayuda: prevPoint.distancia(point)
             double distance = prevPoint.distancia(point);
             perimeter += distance;
